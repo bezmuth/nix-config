a60dd81980c17274c9983237e3e85ae32c0e39b7
bezmuth <benkel97@protonmail.com>
2022-08-24 00:09:31 +0100

    Added devshell (also some refactoring)

--- flake.nix
+++ flake.nix
@@ -1,14 +1,29 @@
 {
-  description = "NixOS configuration";
+  description = "Bezmuth's Nixos configuration, here be dragons";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
+    devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, nix-doom-emacs, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, nix-doom-emacs, flake-utils, devshell, ...}:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [devshell.overlay];
+      };
+    in
+    {
+    devShells."${system}".default =
+      pkgs.devshell.mkShell {
+        imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+    };
+
     nixosConfigurations = {
       Mishim = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
