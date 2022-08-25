995512115459bde8ff51b86b3513368fe4ad053e
bezmuth <benkel97@protonmail.com>
2022-08-25 02:31:27 +0100

    Added tailscale

--- flake.nix
+++ flake.nix
@@ -8,9 +8,10 @@
     nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
     devshell.url = "github:numtide/devshell";
     flake-utils.url = "github:numtide/flake-utils";
+    agenix.url = "github:ryantm/agenix";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, nix-doom-emacs, flake-utils, devshell, ...}:
+  outputs = inputs@{ nixpkgs, home-manager, nix-doom-emacs, flake-utils, devshell, agenix, ...}:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -29,6 +30,7 @@
         system = "x86_64-linux";
         modules = [
           ./machines/mishim
+          agenix.nixosModule
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
@@ -47,6 +49,30 @@
           }
         ];
       };
+
+      Salas = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./machines/salas
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.bezmuth =  { pkgs, ... }: {
+              imports = [ nix-doom-emacs.hmModule ./home/home.nix ];
+              programs.doom-emacs = {
+                enable = true;
+                doomPrivateDir = ./home/doom.d;
+                emacsPackage = pkgs.emacsNativeComp;
+              };
+            };
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
+      };
+
     };
   };
 }
--- machines/mishim/default.nix
+++ machines/mishim/default.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, agenix, ... }:
 {
   imports =
     [
@@ -97,6 +97,38 @@
     gnomeExtensions.nasa-apod
     gnomeExtensions.syncthing-indicator
   ];
+  services.tailscale.enable = true;
+  services.openssh.enable = true;
+
+  # load tskey secret
+  age.secrets.tskey.file = ../../secrets/tskey.age;
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up -authkey ${config.age.secrets.tskey.path}
+    '';
+  };
+
 
   programs.kdeconnect.enable = true;
   programs.kdeconnect.package = pkgs.gnomeExtensions.gsconnect;
