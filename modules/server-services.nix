83629e0f5cdf84fcda924db58bc5fdc6d11d5948
bezmuth <benkel97@protonmail.com>
2024-12-25 17:45:54 +0000

    Fix nginx configs

--- modules/server-services.nix
+++ modules/server-services.nix
@@ -37,12 +37,14 @@
         ;
       };
       locations."/shiori" = {
-        proxyPass = "http://127.0.0.1:10001";
+        proxyPass = "http://127.0.0.1:10001/";
         proxyWebsockets = true; # needed if you need to use WebSocket
         extraConfig =
-          # required when the target is also TLS server with multiple hosts
-          # required when the server wants to use HTTP Authentication
-          "proxy_pass_header Authorization;"
+          "proxy_set_header Host $host;"
+            "proxy_set_header X-Real-IP $remote_addr;"
+            # required when the target is also TLS server with multiple hosts
+            # required when the server wants to use HTTP Authentication
+            "proxy_pass_header Authorization;"
         ;
       };
       locations."/calibre" = {
@@ -52,6 +54,10 @@
           # required when the target is also TLS server with multiple hosts
           # required when the server wants to use HTTP Authentication
           "proxy_pass_header Authorization;"
+            "proxy_set_header        Host            $http_host;"
+            "proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;"
+            "proxy_set_header        X-Scheme        $scheme;"
+            "proxy_set_header        X-Script-Name   /calibre"  # IMPORTANT: path has NO trailing slash
         ;
       };
     };
