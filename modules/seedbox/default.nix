4e26150fb43103456d3d155ce10875c38a380672
bezmuth <benkel97@protonmail.com>
2024-12-28 01:42:40 +0000

    Fix transmission

--- modules/seedbox/default.nix
+++ modules/seedbox/default.nix
@@ -1,13 +1,19 @@
 {...}: {
   imports = [./container.nix];
   services = {
-    caddy = {
+    nginx = {
       enable = true;
+      recommendedProxySettings = true;
+      # other Nginx options
       virtualHosts."127.0.0.1" = {
-        extraConfig = ''
-          http_port 9091
-          reverse_proxy http://127.0.0.1:9091
-        '';
+        locations."/transmission" = {
+          proxyPass = "http://127.0.0.1:9091";
+          proxyWebsockets = true; # needed if you need to use WebSocket
+          extraConfig =
+            # required when the target is also TLS server with multiple hosts
+            # required when the server wants to use HTTP Authentication
+            "proxy_pass_header Authorization;";
+        };
       };
     };
   };
