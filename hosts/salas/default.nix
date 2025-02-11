643864bd668be150692892bc8e5032293d424b32
bezmuth <benkel97@protonmail.com>
2025-02-11 17:26:23 +0000

    Flake: update

--- hosts/salas/default.nix
+++ hosts/salas/default.nix
@@ -55,11 +55,11 @@ args @ {
       enable = true;
       settings = {
         Macs = [
-        "hmac-sha2-512-etm@openssh.com"
-        "hmac-sha2-256-etm@openssh.com"
-        "umac-128-etm@openssh.com"
-        "hmac-sha2-512"
-      ];
+          "hmac-sha2-512-etm@openssh.com"
+          "hmac-sha2-256-etm@openssh.com"
+          "umac-128-etm@openssh.com"
+          "hmac-sha2-512"
+        ];
         KbdInteractiveAuthentication = false;
         PasswordAuthentication = false;
       };
