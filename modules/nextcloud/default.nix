3baaefacdddce1cdd80427926f68243c8415c5bb
bezmuth <benkel97@protonmail.com>
2025-02-23 17:41:52 +0000

    Nextcloud: docker to native

--- modules/nextcloud/default.nix
+++ modules/nextcloud/default.nix
@@ -1,25 +1,54 @@
 {
+  pkgs,
+  config,
+  localPort ? 0,
   url ? "nextcloud.bezmuth.uk",
   acmeHost ? "bezmuth.uk",
   ...
 }: {
-  imports = [./container.nix];
-  services.caddy = {
-    enable = true;
-    virtualHosts."${url}" = {
-      useACMEHost = "${acmeHost}";
-      extraConfig = ''
-        reverse_proxy 172.19.0.2:443 {
-          transport http {
-            tls_insecure_skip_verify
-          }
-        }
-        header {
-          Strict-Transport-Security max-age=31536000;
-        }
-        redir /.well-known/webfinger /public.php?service=webfinger 301
-        bind 100.103.106.16
-      '';
+  services = {
+    nextcloud = {
+      enable = true;
+      configureRedis = true;
+      package = pkgs.nextcloud30;
+      hostName = url;
+      maxUploadSize = "20G";
+      config = {
+        dbtype = "sqlite";
+        adminpassFile = "/nextcloud.txt";
+        adminuser = "bezmuth";
+      };
+      settings = {
+        trusted_proxies = ["localhost" "127.0.0.1" "100.103.106.16" "nextcloud.bezmuth.uk"];
+        trusted_domains = ["nextcloud.bezmuth.uk"];
+        overwriteprotocol = "https";
+      };
+      extraApps = {
+        inherit (config.services.nextcloud.package.packages.apps) contacts bookmarks notes phonetrack previewgenerator;
+      };
+      extraAppsEnable = true;
+    };
+    nginx.virtualHosts."${url}".listen = [
+      {
+        addr = "127.0.0.1";
+        port = localPort;
+      }
+    ];
+    caddy = {
+      enable = true;
+      virtualHosts."${url}" = {
+        useACMEHost = "${acmeHost}";
+        extraConfig = ''
+          redir /.well-known/carddav /remote.php/dav 301
+          redir /.well-known/caldav /remote.php/dav 301
+          redir /.well-known/webfinger /index.php/.well-known/webfinger 301
+          redir /.well-known/nodeinfo /index.php/.well-known/nodeinfo 301
+
+          encode gzip
+          reverse_proxy 127.0.0.1:${builtins.toString localPort}
+          bind 100.103.106.16
+        '';
+      };
     };
   };
 }
