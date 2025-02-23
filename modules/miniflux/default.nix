52a7c3f2b1a8e99f7db7335dd8206a7cd75ab7c2
bezmuth <benkel97@protonmail.com>
2025-02-23 18:01:40 +0000

    Miniflux: switch embed

--- modules/miniflux/default.nix
+++ modules/miniflux/default.nix
@@ -21,7 +21,6 @@
       config = {
         LISTEN_ADDR = "localhost:${builtins.toString localPort}";
         FETCH_YOUTUBE_WATCH_TIME = "1";
-        YOUTUBE_EMBED_URL_OVERRIDE = "https://inv.nadeko.net/embed/";
       };
       adminCredentialsFile = "/home/bezmuth/miniflux.txt";
     };
