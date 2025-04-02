19fb0dedca5c6b74c0619820ed0171fbe6f38736
bezmuth <benkel97@protonmail.com>
2025-04-02 20:53:27 +0100

    Salas: rmfakecloud

---
+++ modules/rmfakecloud/default.nix
@@ -0,0 +1,25 @@
+{
+  localPort ? 0,
+  url ? "rm.bezmuth.uk",
+  acmeHost ? "bezmuth.uk",
+  ...
+}:
+{
+  services = {
+    rmfakecloud = {
+      enable = true;
+      storageUrl = "https://${url}";
+      port = localPort;
+    };
+    caddy = {
+      enable = true;
+      virtualHosts."${url}" = {
+        useACMEHost = acmeHost;
+        extraConfig = ''
+          reverse_proxy http://127.0.0.1:${builtins.toString localPort}
+          bind 100.103.106.16
+        '';
+      };
+    };
+  };
+}
