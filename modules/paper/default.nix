11437e71f5ee599e3d5e584c78bc5e76df7ffe73
bezmuth <benkel97@protonmail.com>
2025-07-07 18:18:08 +0100

    Salas: mc enable krypton

--- modules/paper/default.nix
+++ modules/paper/default.nix
@@ -55,10 +55,10 @@
             #   url = "https://cdn.modrinth.com/data/uXXizFIs/versions/CtMpt7Jr/ferritecore-8.0.0-fabric.jar";
             #   hash = "sha256-K5C/AMKlgIw8U5cSpVaRGR+HFtW/pu76ujXpxMWijuo=";
             # };
-            # Krypton = pkgs.fetchurl {
-            #   url = "https://cdn.modrinth.com/data/fQEb0iXm/versions/neW85eWt/krypton-0.2.9.jar";
-            #   hash = "sha256-uGYia+H2DPawZQxBuxk77PMKfsN8GEUZo3F1zZ3MY6o=";
-            # };
+            Krypton = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/fQEb0iXm/versions/neW85eWt/krypton-0.2.9.jar";
+              hash = "sha256-uGYia+H2DPawZQxBuxk77PMKfsN8GEUZo3F1zZ3MY6o=";
+            };
             # Lithium = pkgs.fetchurl {
             #   url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/77EtzYFA/lithium-fabric-0.18.0%2Bmc1.21.7.jar";
             #   hash = "sha256-uUwXEFqZaZvikSv2LbfjT+LwJjJH1JguRAzwpmEwJqE=";
