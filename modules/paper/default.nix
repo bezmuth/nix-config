ed32d83b580263d4f03b07af13298f0313c4977b
bezmuth <benkel97@protonmail.com>
2025-07-26 22:14:21 +0100

    Update mods and mc server

--- modules/paper/default.nix
+++ modules/paper/default.nix
@@ -30,32 +30,27 @@
       };
 
       # Specify the custom minecraft server package
-      package = pkgs.fabricServers.fabric-1_21_7;
+      package = pkgs.fabricServers.fabric-1_21_8;
 
       symlinks = {
         mods = pkgs.linkFarmFromDrvs "mods" (
           builtins.attrValues {
             Fabric-API = pkgs.fetchurl {
-              url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/JIZogEYa/fabric-api-0.128.2%2B1.21.7.jar";
-              hash = "sha256-YVCRbpbouYCX+k7BYhgparrE6N4Jcwfq1q2e/GfKTQ4=";
+              url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/zhzhM2yQ/fabric-api-0.130.0%2B1.21.8.jar";
+              hash = "sha256-k/+F7dZLgCltzTcttFJaMzJoDKnAeQ4o6StVBSkBdYk=";
             };
-            # Distant-Horizons = pkgs.fetchurl {
-            #   url = "https://cdn.modrinth.com/data/uCdwusMi/versions/Mt9bDAs6/DistantHorizons-neoforge-fabric-2.3.2-b-1.21.5.jar";
-            #   hash = "sha256-HJixoCVz/Xs9vkd6jha+wUxGiDqT7F0eQkvKAY4+yEs=";
-            # };
-            # Tick-Stasis = pkgs.fetchurl {
-            #   url = "https://cdn.modrinth.com/data/t6XBQ2xn/versions/fDbxgNHz/tick-stasis-1.1.1.jar";
-            #   hash = "sha256-BYGJG1GOy+WLQhSRmJNMRmlG1QWjUbd7Jyffwhvf5cY=";
-            # };
-
+            Distant-Horizons = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/uCdwusMi/versions/9yaYzpcr/DistantHorizons-2.3.4-b-1.21.8-fabric-neoforge.jar";
+              hash = "sha256-rbtFZ1PzCojLTtyiVpb0EJcbvSkl8xAr4Ae2bxuLoY8=";
+            };
+            Tick-Stasis = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/t6XBQ2xn/versions/fDbxgNHz/tick-stasis-1.1.1.jar";
+              hash = "sha256-BYGJG1GOy+WLQhSRmJNMRmlG1QWjUbd7Jyffwhvf5cY=";
+            };
             # Optimises a lot of the same things as lithium and ferrite core, I think it was causing breakage
             Moonrise = pkgs.fetchurl {
-              url = "https://cdn.modrinth.com/data/KOHu7RCS/versions/deKWI7rK/Moonrise-Fabric-0.5.0-beta.1%2B90ce197.jar";
-              hash = "sha256-0Go7hZOUG9mel0J/dJp21cT8r3fJoTqL4YsaXFA+BWs=";
-            };
-            Krypton = pkgs.fetchurl {
-              url = "https://cdn.modrinth.com/data/fQEb0iXm/versions/neW85eWt/krypton-0.2.9.jar";
-              hash = "sha256-uGYia+H2DPawZQxBuxk77PMKfsN8GEUZo3F1zZ3MY6o=";
+              url = "https://cdn.modrinth.com/data/KOHu7RCS/versions/70KoSrYl/Moonrise-Fabric-0.6.0-beta.1%2B45edfd7.jar";
+              hash = "sha256-kVRkXtnxDojsFnBj0FXQ0rjDbQNn4PEyfD9ilaihvj4=";
             };
           }
         );
