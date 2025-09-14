921e855d5f5363f85ff9ce10ffbc929f00d5f24f
bezmuth <benkel97@protonmail.com>
2025-09-14 17:54:35 +0100

    Salas: add matrix and bridges

---
+++ modules/ntfy/default.nix
@@ -0,0 +1,27 @@
+{
+  localPort ? 0,
+  url ? "ntfy.bezmuth.uk",
+  acmeHost ? "bezmuth.uk",
+  ...
+}:
+{
+  services = {
+    ntfy-sh = {
+      enable = true;
+      settings = {
+        base-url = url;
+        listen-http = ":${builtins.toString localPort}";
+      }
+    }
+    caddy = {
+      enable = true;
+      virtualHosts."${url}" = {
+        useACMEHost = acmeHost;
+        extraConfig = ''
+          reverse_proxy 127.0.0.1:${builtins.toString localPort}
+          bind 100.103.106.16
+        '';
+      };
+    };
+  };
+}
