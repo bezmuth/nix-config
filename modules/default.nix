b769197ea56a85285332fe9f925171d6b7f08750
benkel97@protonmail.com <benkel97@protonmail.com>
2025-12-11 02:47:49 +0000

    Disable (some) setuid binaries

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,7 @@
 # common config between "all" (roshar and mishim) devices
 {
   pkgs,
+  lib,
   ...
 }:
 {
@@ -65,6 +66,19 @@
   security = {
     polkit.enable = true;
     sudo.enable = false;
+    wrappers = {
+      su.setuid = lib.mkForce false;
+      sg.setuid = lib.mkForce false;
+      fusermount.setuid = lib.mkForce false;
+      fusermount3.setuid = lib.mkForce false;
+      mount.setuid = lib.mkForce false;
+      umount.setuid = lib.mkForce false;
+      pkexec.setuid = lib.mkForce false;
+      newgrp.setuid = lib.mkForce false;
+      newgidmap.setuid = lib.mkForce false;
+      newuidmap.setuid = lib.mkForce false;
+      chsh.setuid = lib.mkForce false;
+    };
     pki.certificates = [
       ''
         -----BEGIN CERTIFICATE-----
