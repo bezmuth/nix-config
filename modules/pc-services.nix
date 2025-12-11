5b2edfb5a551d8dd2b2ff7ada5952f66eb312174
benkel97@protonmail.com <benkel97@protonmail.com>
2025-12-11 02:34:47 +0000

    Add polkit graphical agent

--- modules/pc-services.nix
+++ modules/pc-services.nix
@@ -5,6 +5,20 @@
   security.pam.sshAgentAuth.enable = true;
   security.rtkit.enable = true;
 
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+
   virtualisation = {
     libvirtd.enable = true;
     spiceUSBRedirection.enable = true;
